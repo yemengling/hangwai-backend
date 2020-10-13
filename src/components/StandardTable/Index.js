5f3c62ca247d6e5a45f36731770ca3d51e69ffcf
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-13 19:02:24 +0800

    Add files via upload

---
+++ src/components/StandardTable/Index.js
@@ -0,0 +1,63 @@
+import React, { PureComponent } from 'react';
+import { Table, Pagination } from 'antd';
+import styles from './index.less';
+
+class StandardTable extends PureComponent {
+    constructor(props) {
+        super(props);
+        // const { columns } = props;
+        // const needTotalList = initTotalList(columns);
+
+        // this.state = {
+        //     selectedRowKeys: [],
+        //     needTotalList,
+        // };
+    }
+
+    onShowSizeChang = (current, pageSize) => {
+        const { onShowSizeChange } = this.props;
+        
+        onShowSizeChange(current, pageSize);
+        console.log(current, pageSize);
+    }
+
+    onChang = (pageNumber) => {
+        const { onChange } = this.props;
+
+        onChange(pageNumber);
+        console.log('Page: ', pageNumber);
+    }
+
+    render() {
+        const { scroll, rowKey, data, ...rest } = this.props;
+        const { showSizeChanger, defaultCurrent, defaultPageSize, current, total } = this.props;
+
+        return (
+            <div className={styles.standardTable}>
+                <Table
+                    scroll={scroll && scroll}
+                    rowKey={rowKey || 'key'}
+                    dataSource={data}
+                    pagination={false}
+                    {...rest}
+                />
+                <div className={styles.pagination}>
+                    <Pagination 
+                        showQuickJumper
+                        hideOnSinglePage={true}
+                        showSizeChanger={showSizeChanger}
+                        defaultCurrent={defaultCurrent ? defaultCurrent : 1}
+                        defaultPageSize={defaultPageSize ? defaultPageSize : 10}
+                        current={current ? current : 1}
+                        total={total}
+                        showTotal={total => `总共 ${total} 条记录`}
+                        onChange={this.onChang}
+                        onShowSizeChange={this.onShowSizeChang}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StandardTable;
