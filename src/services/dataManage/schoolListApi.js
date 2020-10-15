76cd3e6280aa96aacf9f2dc05f3183cc336a7920
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-15 17:06:52 +0800

    Add files via upload

--- src/services/dataManage/schoolListApi.js
+++ src/services/dataManage/schoolListApi.js
@@ -3,11 +3,35 @@ import request from '@/utils/request';
 
 // 获取列表
 export const getSchoolList = (params) => {
-  let url = `/?`;
+  let url = `/read?`;
 
   const valueArr = ['pageSize', 'pageIndex'];
 
   url = buildUrl({ url, valueArr, value: params });
 
   return request(url);
+};
+
+// 新增
+export const addSchoolInfo = (params) => {
+  return request('/add', {
+    method: "POST",
+    body: params
+  })
+};
+
+// 编辑
+export const updateSchoolInfo = (params) => {
+  return request('/update', {
+    method: "POST",
+    body: params
+  })
+};
+
+// 删除
+export const deleteSchoolInfo = (params) => {
+  return request('/delete', {
+    method: "POST",
+    body: params
+  })
 };
