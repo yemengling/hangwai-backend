fc27a777b336a386d9e76899d9e94db016cbae4d
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-15 17:21:03 +0800

    Add files via upload

--- src/models/exGlobal.js
+++ src/models/exGlobal.js
@@ -2,5 +2,27 @@ export default {
     namespace: 'exGlobal',
     state: {
         menuData: []
+    },
+    effects: {
+        * getCurrentUser(_, {put, call}) {
+            const response = yield call(getCurrentUser);
+
+            yield put({
+                type: 'saveMenuData',
+                payload: {
+                    pageList: data
+                }
+            });
+        }
+    },
+    reducers: {
+        saveMenuData(state, { payload }) {
+            const menuDataArr = changeToMenuData(payload);
+
+            return {
+                ...state,
+                menuData: menuDataArr
+            }
+        }
     }
 }
