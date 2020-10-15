9e5cfb5f223df6c0419ac43fddf326186903db49
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-15 17:04:56 +0800

    Add files via upload

---
+++ src/pages/schoolList/sub/UpdateSchoolListView.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Form } from 'antd';
+import { connect } from 'dva';
+import { schoolListFieldName } from "@/pages/dataManage/schoolList/SchoolList";
+import ShowModals from "@/components/Modals";
+
+const config = {
+    labelCol: 7,
+    wrapperCol: 16
+};
+
+const UpdateStudentListView = (props) => {
+    const { modalVisible, title, recordData, dispatch, okHandle, onCancel } = props;
+    const [form] = Form.useForm();
+
+    const element = [
+        {
+            eleName: 'Input',
+            title: schoolListFieldName['city'],
+            name: 'city',
+            initialValue: recordData.city,
+            spanNum: 24,
+            rules: [
+                {
+                    required: true
+                }
+            ]
+        },
+        {
+            eleName: 'Input',
+            title: schoolListFieldName['school'],
+            name: 'school',
+            initialValue: recordData.school,
+            spanNum: 24,
+            rules: [
+                {
+                    required: true
+                }
+            ]
+        }
+    ];
+
+    return (
+        <ShowModals
+            config={config && config}
+            form={form}
+            modalVisible={modalVisible && modalVisible}
+            title={title && title}
+            element={element && element}
+            okHandle={okHandle && okHandle}
+            onCancel={onCancel && onCancel}
+        />
+    )
+};
+
+export default connect(({ loading, exGlobal }) => ({
+    loading: loading.models.exGlobal,
+    exGlobal
+}))(UpdateStudentListView);
