0cbe5c20e084ee417c8be4a69cf9f1ef477bc42c
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-27 10:23:23 +0800

    Add files via upload

--- src/models/exGlobal.js
+++ src/models/exGlobal.js
@@ -1,10 +1,14 @@
+import { getCityList, getSchoolList } from "@/services/exGlobalApi";
+
 export default {
     namespace: 'exGlobal',
     state: {
-        menuData: []
+        menuData: [],
+        cityList: [],
+        schoolList: []
     },
     effects: {
-        * getCurrentUser(_, {put, call}) {
+        * getCurrentUser(_, { put, call }) {
             const response = yield call(getCurrentUser);
 
             yield put({
@@ -12,6 +16,42 @@ export default {
                 payload: {
                     pageList: data
                 }
+            });
+        },
+        * getCityList(_, { put, call }) {
+            // const response = yield call(getCityList);
+
+            const response = {
+                code: 0,
+                data: [
+                    {
+                        id: 1,
+                        text: '城区1'
+                    }
+                ]
+            }
+
+            yield put({
+                type: 'saveCityList',
+                payload: response.data
+            });
+        },
+        * getSchoolList(_, { put, call }) {
+            // const response = yield call(getSchoolList);
+
+            const response = {
+                code: 0,
+                data: [
+                    {
+                        id: 1,
+                        text: '学校1'
+                    }
+                ]
+            }
+
+            yield put({
+                type: 'saveSchoolList',
+                payload: response.data
             });
         }
     },
@@ -23,6 +63,18 @@ export default {
                 ...state,
                 menuData: menuDataArr
             }
+        },
+        saveCityList(state, { payload }) {
+            return {
+                ...state,
+                cityList: payload
+            };
+        },
+        saveSchoolList(state, { payload }) {
+            return {
+                ...state,
+                schoolList: payload
+            };
         }
     }
 }
