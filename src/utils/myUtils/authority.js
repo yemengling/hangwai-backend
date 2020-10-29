2f772bad5003972a06a234e8541c9465c5a9273b
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-29 20:31:13 +0800

    Add files via upload

--- src/utils/myUtils/authority.js
+++ src/utils/myUtils/authority.js
@@ -1,34 +1,27 @@
 // 获取表的操作权限数组
 export const getAuthorityOpreateArea = (menuData, listAuth) => {
-  let arr = [];
+  let asOperation = [];
 
   for (let i = 0; i < menuData.length; i++) {
-    if (menuData[i].pageNameEn === listAuth) {
-      // 返回里面的opre
-      if (menuData[i].opre) {
-        arr = menuData[i].opre;
-        return arr;
-      } else {
-        return arr;
-      }
-    }
+    if (menuData[i].pageEnName === listAuth) {
+      asOperation = menuData[i].operation;
+    } else if (menuData[i].children) {
+      asOperation = getAuthorityOpreateArea(menuData[i].children, listAuth);
 
-    if (menuData[i].children) {
-      arr = getAuthorityOpreateArea(menuData[i].children, listAuth);
-      if (arr.length > 0) {
-        return arr;
+      if(asOperation.length > 0){
+        return asOperation;
       }
     }
   }
-
-  return arr;
+  
+  return asOperation;
 };
 
 // 具体是否可以操作
 export const getAuthorityOpreatDetail = (areaArr, opreAuth) => {
   for (let i in areaArr) {
     for (let key in opreAuth) {
-      if (areaArr[i].pageNameEn === opreAuth[key]) {
+      if (areaArr[i].pageEnName === opreAuth[key]) {
         opreAuth[key] = true;
         break;
       }
