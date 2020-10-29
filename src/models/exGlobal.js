1631f8dc2d14b10bd16a6f7ec841223509fe9702
yemengling <32728512+yemengling@users.noreply.github.com>
2020-10-29 20:30:35 +0800

    Add files via upload

--- src/models/exGlobal.js
+++ src/models/exGlobal.js
@@ -1,21 +1,104 @@
-import { getCityList, getSchoolList } from "@/services/exGlobalApi";
+import { getMenuData, getCityList, getSchoolList, getRoleList } from "@/services/exGlobalApi";
 
 export default {
     namespace: 'exGlobal',
     state: {
         menuData: [],
         cityList: [],
-        schoolList: []
+        schoolList: [],
+        roleList: [],
     },
     effects: {
-        * getCurrentUser(_, { put, call }) {
-            const response = yield call(getCurrentUser);
+        * getMenuData(_, { put, call }) {
+            // const response = yield call(getMenuData);
+
+            const response = {
+                code: 0,
+                data: [
+                    {
+                        pageEnName: 'dataManage',
+                        children: [
+                            {
+                                pageEnName: 'schoolList',
+                                operation: [
+                                    {
+                                        name: "新增",
+                                        pageEnName: "schoolList_add"
+                                    },
+                                    {
+                                        name: "编辑",
+                                        pageEnName: "schoolList_update"
+                                    },
+                                    {
+                                        name: "删除",
+                                        pageEnName: "schoolList_delete"
+                                    }
+                                ]
+                            },
+                            {
+                                pageEnName: 'studentList',
+                                operation: [
+                                    {
+                                        name: "新增",
+                                        pageEnName: "studentList_add"
+                                    },
+                                    {
+                                        name: "编辑",
+                                        pageEnName: "studentList_update"
+                                    },
+                                    {
+                                        name: "删除",
+                                        pageEnName: "studentList_delete"
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        pageEnName: 'systemManage',
+                        children: [
+                            {
+                                pageEnName: 'roleList',
+                                operation: [
+                                    {
+                                        name: "新增",
+                                        pageEnName: "roleList_add"
+                                    },
+                                    {
+                                        name: "编辑",
+                                        pageEnName: "roleList_update"
+                                    },
+                                    {
+                                        name: "删除",
+                                        pageEnName: "roleList_delete"
+                                    }
+                                ]
+                            },
+                            {
+                                pageEnName: 'accountList',
+                                operation: [
+                                    {
+                                        name: "新增",
+                                        pageEnName: "accountList_add"
+                                    },
+                                    {
+                                        name: "编辑",
+                                        pageEnName: "accountList_update"
+                                    },
+                                    {
+                                        name: "删除",
+                                        pageEnName: "accountList_delete"
+                                    }
+                                ]
+                            },
+                        ]
+                    }
+                ]
+            }
 
             yield put({
                 type: 'saveMenuData',
-                payload: {
-                    pageList: data
-                }
+                payload: response.data
             });
         },
         * getCityList(_, { put, call }) {
@@ -53,15 +136,31 @@ export default {
                 type: 'saveSchoolList',
                 payload: response.data
             });
+        },
+        * getRoleList(_, { put, call }) {
+            // const response = yield call(getSchoolList);
+
+            const response = {
+                code: 0,
+                data: [
+                    {
+                        id: 1,
+                        text: '角色1'
+                    }
+                ]
+            }
+
+            yield put({
+                type: 'saveRoleList',
+                payload: response.data
+            });
         }
     },
     reducers: {
         saveMenuData(state, { payload }) {
-            const menuDataArr = changeToMenuData(payload);
-
             return {
                 ...state,
-                menuData: menuDataArr
+                menuData: payload
             }
         },
         saveCityList(state, { payload }) {
@@ -74,6 +173,12 @@ export default {
             return {
                 ...state,
                 schoolList: payload
+            };
+        },
+        saveRoleList(state, { payload }) {
+            return {
+                ...state,
+                roleList: payload
             };
         }
     }
