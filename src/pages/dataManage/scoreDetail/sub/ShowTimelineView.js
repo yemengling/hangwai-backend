a748103cd26bf08dc3397895fb6617bc70c39716
yemengling <32728512+yemengling@users.noreply.github.com>
2021-02-20 20:05:13 +0800

    Add files via upload

--- src/pages/dataManage/scoreDetail/sub/ShowTimelineView.js
+++ src/pages/dataManage/scoreDetail/sub/ShowTimelineView.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import { Chart, LineAdvance } from 'bizcharts';
+import {
+	Chart,
+	LineAdvance
+} from 'bizcharts';
 
 const ShowTimelineView = (props) => {
-  // 数据源
-  const { data } = props;
+	// 数据源
+	const { data } = props;
 
-  return (
-    <div style={{ background: '#fff', padding: '20px' }}>
-      <h2>学生成绩变化</h2>
-      <Chart
-        autoFit
-        padding={[10, 20, 60, 40]}
-        height={300}
-        data={data}
-        scale={{
-          score: {
-            min: 0,
-          },
-        }}
-      >
-        <LineAdvance shape="smooth" point area position="name*score" color="paper" />
-      </Chart>
-    </div>
-  );
-};
+	return (
+		<div style={{ "background": "#fff", "padding": "20px" }}>
+			<h2>学生成绩变化</h2>
+			<Chart autoFit padding={[10, 20, 60, 40]} height={300}
+				data={data}
+				scale={{
+					score: {
+						min: 0
+					}
+				}}
+			>
+				<LineAdvance
+					shape="smooth"
+					point
+					area
+					position="name*score"
+					color="paper"
+				/>
+			</Chart>
+		</div>
+	)
+}
 
 export default ShowTimelineView;
