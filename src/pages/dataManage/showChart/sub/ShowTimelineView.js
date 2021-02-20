698071b685344b5d4994ea7314d96a38e9683161
yemengling <32728512+yemengling@users.noreply.github.com>
2021-02-20 20:06:12 +0800

    Add files via upload

--- src/pages/dataManage/showChart/sub/ShowTimelineView.js
+++ src/pages/dataManage/showChart/sub/ShowTimelineView.js
@@ -1,37 +1,48 @@
 import React from 'react';
-import { Chart, LineAdvance } from 'bizcharts';
+import {
+    Chart,
+    LineAdvance
+} from 'bizcharts';
 
 const ShowTimelineView = (props) => {
-  // 数据源
-  const { title, type, data } = props;
+    // 数据源
+    const { title, type, data } = props;
 
-  return (
-    <React.Fragment>
-      <div style={{ paddingBottom: '50px' }}>
-        <h2>{title}</h2>
-        <Chart
-          autoFit
-          padding={[10, 20, 60, 40]}
-          height={300}
-          data={data}
-          scale={{
-            people: {
-              min: 0,
-            },
-            score: {
-              min: 0,
-            },
-          }}
-        >
-          {type === '1' ? (
-            <LineAdvance shape="smooth" point area position="school*people" />
-          ) : (
-            <LineAdvance shape="smooth" point area position="name*score" color="paper" />
-          )}
-        </Chart>
-      </div>
-    </React.Fragment>
-  );
-};
+    return (
+        <React.Fragment>
+            <div style={{ paddingBottom: "50px" }}>
+                <h2>{title}</h2>
+                <Chart autoFit padding={[10, 20, 60, 40]} height={300}
+                    data={data}
+                    scale={{
+                        people: {
+                            min: 0
+                        },
+                        score: {
+                            min: 0
+                        }
+                    }}
+                >
+                    {
+                        type == "1" ?
+                            <LineAdvance
+                                shape="smooth"
+                                point
+                                area
+                                position="school*people"
+                            /> :
+                            <LineAdvance
+                                shape="smooth"
+                                point
+                                area
+                                position="name*score"
+                                color="paper"
+                            />
+                    }
+                </Chart>
+            </div>
+        </React.Fragment>
+    )
+}
 
 export default ShowTimelineView;
